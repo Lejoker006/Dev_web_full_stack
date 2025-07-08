f27025e574639885c99e2687e37f1ef63acbe639
Lejoker006 <168846343+Lejoker006@users.noreply.github.com>
2025-07-08 19:15:57 +0200

    Create étape1_2.jsx

---
+++ part1/courseinfo/étape1_2.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+
+const Total = (props) => {
+  return (
+    <div>
+
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+ ]
+
+  return (
+    <div>
+      <Header title = {course}/>
+      <Content parts={parts} />
+      <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+    </div>
+  )
+}
+
+export default App
